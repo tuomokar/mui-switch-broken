e69946c7d0e4cafa475f05f733aea768a2bf5763
Tuomo Oila <tuomo.oila@haahtela.fi>
2021-02-23 11:23:22 +0200

    Display MUI's switch (broken version) and a fixed version of it

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+import styled from '@emotion/styled'
+import Switch from '@material-ui/core/Switch'
+
+const FixedSwitch = styled(Switch)`
+  &&& .MuiIconButton-root {
+        position: absolute;
+    }
+`
+
 function App() {
+  const [state, setState] = useState({
+    fixedSwitch: true,
+    brokenSwitch: true,
+})
+
+const handleChange = (event) => {
+    setState({
+        ...state,
+        [event.target.name]: event.target.checked,
+    })
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Material UI's Switch</h1>
+        
+        <h2>Broken switch</h2>
+        <Switch checked={state.brokenSwitch} onChange={handleChange} name="brokenSwitch" />
+
+        <h2>Fixed switch</h2>
+        <FixedSwitch checked={state.fixedSwitch} onChange={handleChange} name="fixedSwitch" />
       </header>
     </div>
   );
